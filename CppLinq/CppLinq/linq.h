809cb64ae0eb9c7d7a2082e715f452d1d459c3ca
vczh <vczh@163.com>
2014-03-29 10:09:35 +0800

    ...

--- CppLinq/CppLinq/linq.h
+++ CppLinq/CppLinq/linq.h
@@ -2,6 +2,7 @@
 
 #include <utility>
 #include <xutility>
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <vector>
@@ -11,7 +12,6 @@
 #include <unordered_map>
 #include <set>
 #include <unordered_set>
-#include <algorithm>
 
 namespace vczh
 {
