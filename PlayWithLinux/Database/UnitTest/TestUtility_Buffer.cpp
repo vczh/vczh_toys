67d479ec38fa401660458ace826a96236dbc818c
vczh <vczh@163.com>
2014-09-09 00:03:01 +0800

    The simplest C++ unit test framework in the world

---
+++ PlayWithLinux/Database/UnitTest/TestUtility_Buffer.cpp
@@ -0,0 +1,9 @@
+#include "UnitTest.h"
+
+TEST_CASE(Utility_Buffer_FixedSize_InMemory)
+{
+}
+
+TEST_CASE(Utility_Buffer_FixedSize_Disk)
+{
+}
--- PlayWithLinux/Database/UnitTest/UnitTest.h
+++ PlayWithLinux/Database/UnitTest/UnitTest.h
@@ -3,4 +3,18 @@
 
 #include "../Source/DatabaseVlppReferences.h"
 
+#define TEST_CASE(NAME)										\
+	void TestFunction_##NAME(void);							\
+	class TestClass_##NAME									\
+	{														\
+	public:													\
+		TestClass_##NAME()									\
+		{													\
+			vl::console::Console::WriteLine(L ## #NAME);	\
+			TestFunction_##NAME();							\
+		}													\
+	} TestInstance_##NAME;									\
+	void TestFunction_##NAME(void)							\
+
+
 #endif
