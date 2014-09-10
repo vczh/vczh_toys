2f1facd0848c88e3e36e8dd44a6445ce0e453bf8
vczh <vczh@163.com>
2014-09-10 16:44:48 +0800

    BufferManager: source management

--- PlayWithLinux/Database/Source/Utility/Buffer.cpp
+++ PlayWithLinux/Database/Source/Utility/Buffer.cpp
@@ -1,18 +1,27 @@
 #include "Buffer.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 namespace vl
 {
 	namespace database
 	{
+		using namespace collections;
 /***********************************************************************
 BufferManager
 ***********************************************************************/
 
-		BufferManager::BufferManager(vuint64_t _pageSize, vuint64_t _maxCacheSize)
+		BufferManager::BufferManager(vuint64_t _pageSize, vuint64_t _cachePageCount)
 			:pageSize(_pageSize)
-			,maxCacheSize(_maxCacheSize > _pageSize ? _maxCacheSize : _pageSize)
+			,cachePageCount(_cachePageCount)
 			,pageSizeBits(0)
 		{
+			vuint64_t systemPageSize = sysconf(_SC_PAGE_SIZE);
+			pageSize = IntUpperBound(pageSize, systemPageSize);
 			if (pageSize > 0)
 			{
 				pageSizeBits = sizeof(pageSize) * 8;
@@ -25,6 +34,11 @@ BufferManager
 
 		BufferManager::~BufferManager()
 		{
+			while (sourceDescriptions.Count() > 0)
+			{
+				BufferSource source{sourceDescriptions.Keys()[0]};
+				UnloadSource(source);
+			}
 		}
 
 		vuint64_t BufferManager::GetPageSize()
@@ -32,34 +46,79 @@ BufferManager
 			return pageSize;
 		}
 
-		vuint64_t BufferManager::GetMaxCacheSize()
+		vuint64_t BufferManager::GetCachePageCount()
 		{
-			return maxCacheSize;
+			return cachePageCount;
+		}
+
+		vuint64_t BufferManager::GetCacheSize()
+		{
+			return pageSize * cachePageCount;
 		}
 
 		BufferSource BufferManager::LoadMemorySource()
 		{
-			return BufferSource::Invalid();
+			BufferSource source{(BufferSource::IndexType)sourceDescriptions.Count()};
+			SourceDesc desc{true, -1};
+			sourceDescriptions.Add(source.index, desc);
+			return source;
 		}
 
-		BufferSource BufferManager::LoadFileSource(const WString& fileName)
+		BufferSource BufferManager::LoadFileSource(const WString& fileName, bool createNew)
 		{
-			return BufferSource::Invalid();
-		}
+			BufferSource source{(BufferSource::IndexType)sourceDescriptions.Count()};
+			SourceDesc desc{false, -1, fileName};
 
-		BufferSource BufferManager::CreateFileSource(const WString& fileName, bool createNew)
-		{
-			return BufferSource::Invalid();
+			if (createNew)
+			{
+				desc.fileDescriptor = creat(wtoa(fileName).Buffer(), 0x555);
+			}
+			else
+			{
+				desc.fileDescriptor = open(wtoa(fileName).Buffer(), O_RDWR);
+			}
+			if (desc.fileDescriptor == -1)
+			{
+				return BufferSource::Invalid();
+			}
+
+			sourceDescriptions.Add(source.index, desc);
+			return source;
 		}
 
 		bool BufferManager::UnloadSource(BufferSource source)
 		{
-			return false;
+			vint index = sourceDescriptions.Keys().IndexOf(source.index);
+			if (index == -1) return false;
+			auto sourceDesc = sourceDescriptions.Values()[index];
+
+			FOREACH(void*, address, sourceMemories.Get(source.index))
+			{
+				memoryDescriptions.Remove(address);
+				if (sourceDesc.inMemory)
+				{
+					free(address);
+				}
+				else
+				{
+					munmap(address, pageSize);
+				}
+			}
+
+			sourceMemories.Remove(source.index);
+			sourceDescriptions.Remove(source.index);
+			if (!sourceDesc.inMemory)
+			{
+				close(sourceDesc.fileDescriptor);
+			}
+			return true;
 		}
 
 		WString BufferManager::GetSourceFileName(BufferSource source)
 		{
-			return L"";
+			vint index = sourceDescriptions.Keys().IndexOf(source.index);
+			if (index == -1) return L"";
+			return sourceDescriptions.Values()[index].fileName;
 		}
 
 		void* BufferManager::LockPage(BufferSource source, BufferPage page)
--- PlayWithLinux/Database/Source/Utility/Buffer.h
+++ PlayWithLinux/Database/Source/Utility/Buffer.h
@@ -18,6 +18,7 @@ namespace vl
 #define DEFINE_INDEX_TYPE(NAME, TYPE)							\
 		struct NAME												\
 		{														\
+			typedef TYPE		IndexType;						\
 			TYPE				index;							\
 																\
 			bool IsValid()const									\
@@ -40,20 +41,41 @@ namespace vl
 		class BufferManager
 		{
 		private:
+			struct SourceDesc
+			{
+				bool			inMemory;
+				int				fileDescriptor;
+				WString			fileName;
+			};
+			struct PageDesc
+			{
+				void*			address;
+				BufferSource	source;
+				vuint64_t		offset;
+			};
+			typedef collections::Dictionary<BufferSource::IndexType, SourceDesc>	SourceDescMap;
+			typedef collections::Group<BufferSource::IndexType, void*>				SourceMemoryMap;
+			typedef collections::Dictionary<void*, PageDesc>						MemoryDescMap;
+
+		private:
 			vuint64_t			pageSize;
-			vuint64_t			maxCacheSize;
+			vuint64_t			cachePageCount;
 			vuint64_t			pageSizeBits;
 
+			SourceDescMap		sourceDescriptions;
+			SourceMemoryMap		sourceMemories;
+			MemoryDescMap		memoryDescriptions;
+
 		public:
-			BufferManager(vuint64_t _pageSize, vuint64_t _maxCacheSize);
+			BufferManager(vuint64_t _pageSize, vuint64_t _cachePageCount);
 			~BufferManager();
 
 			vuint64_t			GetPageSize();
-			vuint64_t			GetMaxCacheSize();
+			vuint64_t			GetCachePageCount();
+			vuint64_t			GetCacheSize();
 
 			BufferSource		LoadMemorySource();
-			BufferSource		LoadFileSource(const WString& fileName);
-			BufferSource		CreateFileSource(const WString& fileName, bool createNew);
+			BufferSource		LoadFileSource(const WString& fileName, bool createNew);
 			bool				UnloadSource(BufferSource source);
 			WString				GetSourceFileName(BufferSource source);
 
@@ -64,6 +86,12 @@ namespace vl
 			bool				EncodePointer(BufferPointer& pointer, BufferPage page, vuint64_t offset);
 			bool				DecodePointer(BufferPointer pointer, BufferPage& page, vuint64_t& offset);
 		};
+
+		template<typename T>
+		T IntUpperBound(T size, T divisor)
+		{
+			return size + (divisor - (size % divisor)) % divisor;
+		}
 	}
 }
 
